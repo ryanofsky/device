2cc0d7a36d60fe4c26890460e67d0c7aea267045
Russell Yanofsky <russ@yanofsky.org>
2002-12-17 06:24:58 +0000

    - Split up into multiple files: debug.hpp, io.hpp, register.hpp, util.hpp,    and variable.hpp - Removed get and set methods from AtRegister class - Changed reader interface, filled in SplitVariable implementation

--- hello.cpp
+++ hello.cpp
@@ -13,437 +13,96 @@
 using namespace std;
 namespace mpl = boost::mpl;
 
+#include "util.hpp"
+#include "variable.hpp"
+#include "register.hpp"
+#include "io.hpp"
+#include "debug.hpp"
 
-
-struct DebugIO
-{
-  static int nextval;
-  
-  template<typename Register>
-  static void init(Register & reg)
-  {
-    reg.clear();
-  }
-
-  template<typename Device, typename Register>
-  static int read(Device & dev, Register & reg)
-  {
-    cout << "Device " << &dev << ". Reading register "
-         << typeid(Device).name() << ". Value = " << nextval << endl;
-    reg.set(nextval);
-    ++nextval;
-    return 0;
-  }
-
-  template<typename Device, typename Register>  
-  static int write(Device & dev, Register & reg)
-  {
-    int current;
-    reg.get(current);
-    cout << "Device " << &dev << ". Writing register "
-         << NAME << ". Value = " << current << endl;
-    return 0;
-  }
-};
-
-template<typename RegIO, typename IndexVar, int index>
-struct IndexIO
-{
-  template<typename Device, typename Register>
-  static int read(Device & dev, Register & reg)
-  {
-    dev.template set<IndexVar>(index);
-    dev.write<IndexVar>();
-    return RegIO::read(dev, reg);
-  }
-
-  template<typename Device, typename Register>  
-  static int write(Device & dev, Register & reg)
-  {
-    dev.template set<IndexVar>(index);
-    dev.write<IndexVar>();
-    return RegIO::write(dev, reg);
-  }
-};
-
-
-int DebugIO::nextval = 0;
-
-// missing from devil:
-//  bit masks 
-//    bit masks accomplish two things:
-//      runtime constraint checking
-//        express that drivers require certain register bits to be 0 or 1
-//        and allow generated code to check for this when reading and writing
-//        this would be better implemented here in the IO struct
-//
-//      compile time safety and completeness check
-//        mask marks every bit as used or unused
-//        checks that every used bit maps to a variable (completeness)
-//          this would be impossible to implement anyway under the current
-//          framework, because you can't deduce variable types from a 
-//          register type. It is redundant because if you forget
-//          to declare a variable, the code that tries to use the variable
-//          won't compile. 
-//        checks that no unused bit is mapped to a variable (safety)
-//          for this purpose masks provide a useful redundancy
-//         
-//  parameterized registers - using the example in the Devil manual
-//
-//    register reg(i : int(2)) = read base @ 0, pre {index = i} : bit[8];
-//    register x_low = reg(0);
-//
-//    template<int i>
-//    struct reg : Register<8, IndexIO< PortIO<0>, index, i > > {};
-//
-//    typedef reg<0> x_low;
-//      instead of
-//        typedef MyRegister1<32, PortIO<0>
-//
-//  Map Type Equation / Algebraic types:
-//    algebraic types are essentially enumerations, instead of
-//    just being defined by a bit width, they are described as 
-//    lists of possible values. The simplest uses of algebraic t
-//    types are just like C++ enum's. One feature they have
-//    that enum's lack is that possible values may be described
-//    wildcards
-//
-//  variable features missing:
-//    volatile option
-//      causes every device.get<var>()
-//      to be implicity preceded by a device.read<var>()
-//
-//    trigger option
-//      causes every device.set<var>()
-//      to be implicity followed by a device.write<var>()
-//
-// structure serialized as
-//   the default Structure reads and writes from registers
-//   in the order first encountered in the variable list
-//   This should almost always be sufficient. When it is not
-//   simply define a custom structure classed with its
-//   own read() and write() methods. The 
-
-
-template<int BITS, typename IO>
-struct Register
-{
-  enum { IntBits = 8 * sizeof(unsigned int) };
-  enum { CacheDim = (BITS + IntBits - 1)/IntBits };
-  unsigned int cache[CacheDim];
-
-  enum { Bits = BITS };
-  typedef IO Io;
-
-  template<typename INT>
-  void set(INT n, unsigned int pos = 0, unsigned int len = 8*sizeof(INT))
-  {
-    BOOST_STATIC_ASSERT(sizeof(INT) <= sizeof(unsigned int));
-    assert(pos + len <= Bits);
-    assert(len <= IntBits);
-    
-    INT nMask = ~0;
-    n &= ~(nMask << len);
-    
-    int p = pos / IntBits;
-    int offset = pos - p * IntBits;
-    int leftover = offset + len - IntBits;
-
-    unsigned int mask = ~0;
-    unsigned int bits = n;
-    bits <<= offset;
-
-    if (leftover > 0)
-    {
-      cache[p] = (cache[p] & ~(mask << offset)) | bits;
-      ++p;
-      bits = n >> (IntBits - offset);
-      cache[p] = (cache[p] & (mask << leftover)) | bits;
-    }
-    else
-    {
-      unsigned int m = (mask << offset) & (mask >> -leftover);
-      cache[p] = (cache[p] & ~m) | bits;
-    }
-  }
-  
-  template<typename INT>
-  void get(INT & n, unsigned int pos = 0, unsigned int len = 8*sizeof(INT))
-  {
-    assert(pos + len <= Bits);
-    assert(len <= IntBits);
-
-    int p = pos / IntBits;
-    int offset = pos - p * IntBits;
-    int leftover = offset + len - IntBits;
-
-    unsigned int mask = ~0;
-
-    if (leftover > 0)
-    {
-      n = cache[p] >> offset;
-      n |= (cache[p+1] & ~(mask << leftover)) << (IntBits - offset);
-    }
-    else
-    {
-      unsigned int m = (mask << offset) & (mask >> -leftover);
-      n = (cache[p] & (mask >> -leftover)) >> offset;
-    }
-  }
-  
-  void clear()
-  {
-    for (int i = 0; i < CacheDim; ++i)
-      cache[i] = 0;
-  }
-  
-  Register()
-  {
-    IO::init(*this);
-  }
-  
-  template<typename DEVICE>
-  int read(DEVICE & device)
-  {
-    return IO::read(device, *this);
-  }
-
-  template<typename DEVICE>
-  int write(DEVICE & device)
-  {
-    return IO::write(device, *this);
-  }
-};
-
-template<int BITS, typename IO>
-ostream & operator<<(ostream & out, Register<BITS, IO> const & reg)
-{
-  char str[BITS+1];
-  const int d = 8*sizeof(unsigned int);
-  for (int i = 0; i < BITS; ++i)
-    str[BITS-i-1] = (reg.cache[i / d] & 1 << (i % d)) ? '1' : '0';
-  str[BITS] = 0;
-  out << str;
-  return out;
-}
-
-template<typename REGISTER, int FIRSTBIT, int LASTBIT>
-struct Variable
-{
-  typedef unsigned int type;
-
-  typedef REGISTER Register;
-  enum { FirstBit = FIRSTBIT , LastBit = LASTBIT };
-  
-  static void verify()
-  {
-    BOOST_STATIC_ASSERT(Register::Bits > LastBit);
-    BOOST_STATIC_ASSERT(Register::Bits > FirstBit);
-    BOOST_STATIC_ASSERT(LastBit >= FirstBit);
-  }
-  
-  template<typename DEVICE>
-  static type get(DEVICE & dev)
-  {
-    verify();
-    Register & reg = dev.template reg<Register>();
-    type result;
-    reg.get(result, FirstBit, LastBit - FirstBit + 1);
-    return result;
-  }  
-  
-  template<typename DEVICE>
-  static void set(DEVICE & dev, type i)
-  {
-    verify();
-    Register & reg = dev.template reg<Register>();
-    reg.set(i, FirstBit, LastBit - FirstBit + 1);
-  }
-  
-  template<typename DEVICE>
-  static void read(DEVICE & dev)
-  {
-    dev.template reg<Register>().read(dev);
-  }
-  
-
-  template<typename DEVICE>
-  static void write(DEVICE & dev)
-  {
-    dev.template reg<Register>().write(dev);
-  }
-};
-
-
-template<typename T>
-struct is_sequence : public mpl::bool_c<boost::is_same<typename T::tag, mpl::aux::list_tag >::value>
-{};
-
-/*
-
-
-
-
-
-
-
+int NoIO::nextval = 0;
 
 template<typename REGISTER, int FIRSTBIT, int LASTBIT>
 struct AtRegister
 {
   typedef REGISTER Register;
   enum { FirstBit = FIRSTBIT , LastBit = LASTBIT };
-  
+  enum { Width = LASTBIT - FIRSTBIT + 1 };
   static void verify()
   {
     BOOST_STATIC_ASSERT(Register::Bits > LastBit);
     BOOST_STATIC_ASSERT(Register::Bits > FirstBit);
     BOOST_STATIC_ASSERT(LastBit >= FirstBit);
   }  
-
-  template<typename DEVICE>
-  static int type get(DEVICE & dev)
-   v
-    verify();
-    Register & reg = dev.reg<Register>();
-    type result;
-    reg.get(result, FirstBit, LastBit - FirstBit + 1);
-    return result;
-  }  
-  
-  template<typename DEVICE>
-  static void set(DEVICE & dev, type i)
-  {
-    verify();
-    Register & reg = dev.reg<Register>();
-    reg.set(i, FirstBit, LastBit - FirstBit + 1);
-  }
 };
 
 
-template<typename IT, int POS>
+template<typename DEVICE, typename INTEGER>
 struct reader
 {
-  template<typename DEVICE, typename INTEGER>
-  static void type get(DEVICE & dev, INTEGER & i)
+  DEVICE & device;
+  INTEGER & value;
+
+  reader(DEVICE & device_, INTEGER & value_)
+  : device(device_), value(value_) {}
+  
+  template<typename RegInfo, typename Position>
+  void operator()()
   {
-    typedef IT::item Reg;
-    reg::verify();
-    BOOST_STATIC_ASSERT(8*sizeof(INTEGER) >= POS + Reg::LastBit - Reg::FirstBit + 1);
-    
-    Register & reg = dev.reg<Register>();
+    RegInfo::verify();
+    Register & reg = dev.reg<RegInfo::Register>();
     INTEGER result;
-    reg.get(result, Reg::FirstBit, Reg::LastBit - Reg::FirstBit);
-    
-    i |= reg << POS;
-    
-    typedef nextreader;
-    nextreader::get(dev, i);
-    
+    reg.get(result, RegInfo::FirstBit, RegInfo::Width);
+    i |= result << Position::value;
     return;
   }
-  
-}
+};
 
-template<typename IT, int POS>
-struct nullreader
-{
-  template<typename DEVICE, typename INTEGER>
-  static void type get(DEVICE & dev, INTEGER & i)
-  {
-    return;
-  }
-  
-}
+typedef mpl::lambda<mpl::plus<mpl::_1, mpl::_2::Width > >::type inc;
 
 
-using boost::is_same;
+BOOST_STATIC_ASSERT(8*sizeof(INTEGER) >= POS + Reg::LastBit - Reg::FirstBit + 1);
+
 
 template<typename REGISTERS>
 struct SplitVariable
 {
   typedef unsigned int type;
+  typedef mpl::fold<REGISTERS, mpl::int_c<0>, inc>::type Width;
+  
+  static void verify()
+  {
+    BOOST_STATIC_ASSERT(Width::value <= 8*sizeof(type));
+  }
   
   template<typename DEVICE>
-  static int type get(DEVICE & dev)
+  static type get(DEVICE & dev)
   {
-    typedef mpl::begin<REGISTERS>::type It;
-    typedef mpl::if<is_same<>, reader<It,0>, nullreader<It,0> > rd;
-    
-    rd::read
-  
     verify();
-    Register & reg = dev.reg<Register>();
-    type result;
-    reg.get(result, FirstBit, LastBit - FirstBit + 1);
-    return result;
+    type v;
+    reader<DEVICE, type> r(dev, v);
+    for_each_fold<REGISTERS, mpl::int_c<0>, inc>(p);
+    return v;
   }  
 
+  template<typename DEVICE>
+  static void set(DEVICE & dev, type i)
+  {
+    verify();
+    writer<DEVICE, type> r(dev, i);
+    for_each_fold<REGISTERS, mpl::int_c<0>, inc>(p);
+  }  
 
-  
-
-  
   template<typename DEVICE>
   static void read(DEVICE & dev)
   {
     dev.reg<Register>().read(dev);
   }
   
-
   template<typename DEVICE>
   static void write(DEVICE & dev)
   {
     dev.reg<Register>().write(dev);
   }
-};
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-struct EmptyType {};
-
-template <class L, class R>
-struct InheritTwo : public L, public R
-{
-};
-
-
-template<typename T>
-struct Holder
-{
-  T value;
-};
-
-// try replacing this with lambda
-struct ScFun
-{
-  template<typename STATE, typename VAL>
-  struct apply
-  {
-    typedef InheritTwo< Holder<VAL>, STATE > type;
-  };
-};
-
-template<typename Sequence>
-struct HoldClass
-{
-  typedef typename mpl::fold<Sequence, EmptyType, ScFun>::type type;
 };
 
 template<typename REGISTER_LIST, typename ACTUAL>
@@ -471,95 +130,33 @@ struct DeviceBase : public HoldClass<REGISTER_LIST>::type
   }  
 };
 
-template<typename REGISTER_LIST>
-struct Device : public DeviceBase<REGISTER_LIST, Device<REGISTER_LIST> >
-{};
-
 typedef Register<18, DebugIO> ControlReg;
 typedef Variable<ControlReg, 16, 17> High;
 typedef Variable<ControlReg, 0, 7> X;
 typedef Variable<ControlReg, 8, 15> Y;
 
-struct Mouse : DeviceBase< mpl::list1<ControlReg>, Mouse >
+struct Mouse : Device< mpl::list<ControlReg>, Mouse>
 {
-};
-
-template<typename It, typename End>
-struct for_each_fold_impl
-{
-  template<typename State, typename ForwardOp, typename Functor>
-  void operator()(Functor & f)
-  {
-    f.operator()<typename It::type, State>();
-    for_each_fold_impl<typename It::next, End> next;
-    typedef typename mpl::apply2<ForwardOp, State, typename It::type>::type NextState;
-    next.operator()<NextState, ForwardOp>(f);
-  }
-};
-
-template<typename End>
-struct for_each_fold_impl<End, End>
-{
-  template<typename State, typename ForwardOp, typename Functor>
-  void operator()(Functor &)
-  {}
-};
-
-template<typename Sequence, typename State, typename ForwardOp, typename Functor>
-void for_each_fold(Functor & f)
-{
-  for_each_fold_impl<typename mpl::begin<Sequence>::type, typename mpl::end<Sequence>::type> first;
-  first.operator()<State, ForwardOp>(f);
-};
-
-struct Print
-{
-  template<typename T, typename State>
-  void operator()()
-  {
-    cout << "Type " << State::value << ": " << typeid(T).name() << endl;
-  }
 };
 
 int main()
 {
-	typedef mpl::list3<int, short, char> l;
-
-typedef mpl::int_c<22> n0;
-typedef mpl::int_c<13> n1;
-
-typedef mpl::plus<n0, n1> sm;
-
-cout << sm::type::value << endl;
-
-
-	Print p;
-    typedef mpl::lambda<mpl::plus<mpl::_1, mpl::int_c<1> > >::type inc;
-	for_each_fold<l, mpl::int_c<0>, inc>(p);
-
-	
-	
-	
-	//extern char const name[] = "hsll";
-
-/*
   Mouse m;
 
-	cout << m.reg<ControlReg>() << endl;
+  cout << m.reg<ControlReg>() << endl;
   
   m.set<High>(0);
   m.set<X>(715827882);
   m.set<Y>(1431655765);
-
+  
   cout << m.get<X>() << endl;
   cout << m.get<Y>() << endl;
-
-	cout << m.reg<ControlReg>() << endl;
-
+  
+  cout << m.reg<ControlReg>() << endl;
+  
   m.set<Y>(-1);
   
-	cout << m.reg<ControlReg>() << endl;
-*/
+  cout << m.reg<ControlReg>() << endl;
 	
-	return 0;
+  return 0;
 }
