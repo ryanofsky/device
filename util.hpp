552861c8ee2c5c72aebb8e3b21d1ac5dd059f817
Russell Yanofsky <russ@yanofsky.org>
2002-12-21 07:35:20 +0000

    better support for vectors in is_sequence

--- util.hpp
+++ util.hpp
@@ -3,6 +3,13 @@
 
 #include <boost/type_traits/object_traits.hpp>
 
+#   define AUX_VECTOR_PARAMS(param) \
+    BOOST_PP_ENUM_PARAMS( \
+          BOOST_MPL_LIMIT_VECTOR_SIZE \
+        , param \
+        ) \
+
+
 // this is a temporary kludge that will have to suffice until a real
 // is_sequence method gets added to MPL. 
 // It is not implemented using boost's is_convertible type trait
@@ -14,16 +21,18 @@ struct is_sequence
   typedef char Small;
   class Big { char dummy[2]; };
   template<typename VAR, typename par, typename mar>
-  static Small Test(mpl::list_node<VAR, par, mar>);
+  static Small Test(boost::mpl::list_node<VAR, par, mar>);
 
-  // this doesn't detect vector with msvc60
-  // there, vectors inherit from a vector_impl class that takes
-  // different number of types depending on number max params
   template<typename VAR>
-  static Small Test(mpl::vector0<VAR>);
+  static Small Test(boost::mpl::vector0<VAR>);
 
   template<typename VAR, VAR Start, VAR Finish>
-  static Small Test(mpl::range_c<VAR, Start, Finish>);
+  static Small Test(boost::mpl::range_c<VAR, Start, Finish>);
+
+#if defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION)
+  template<AUX_VECTOR_PARAMS(typename U)>
+  static Small Test(boost::mpl::vector_impl<AUX_VECTOR_PARAMS(typename U)>);
+#endif
 
   static Big Test(...);
   static T MakeT();
@@ -106,5 +115,49 @@ struct HoldClass
 {
   typedef typename mpl::fold<Sequence, EmptyType, ScFun>::type type;
 };
+
+//////////////////////////////////////////////////////
+/*
+begin<Sequence>::type
+==
+begin_traits<Sequence::tag>::template algorithm< Sequence >::type
+
+struct subsequence_tag;
+
+typedef<typename begin_, typename end_>
+struct subsequence
+{
+  typedef subsequence_tag;
+  typedef begin_ begin;
+  typedef end_ end;
+};
+
+template<>
+struct boost::mpl::begin_traits<subsequence_tag>
+{
+  template<typename subsequence>
+  struct algorithm
+  {
+    typedef subsequence::begin type;
+  };
+};
+
+template<>
+struct boost::mpl::end_traits<subsequence_tag>
+{
+  template<typename subsequence>
+  struct algorithm
+  {
+    typedef subsequence::end type;
+  };
+};
+
+template<typename Sequence>
+struct Unique
+{
+  typedef mpl::fold_backward<>::type type;
+};
+
+*/
 
 #endif
