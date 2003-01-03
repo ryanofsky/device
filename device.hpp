622cf88109f63e781176202026ac14c443019aaf
Russell Yanofsky <russ@yanofsky.org>
2003-01-03 16:27:12 +0000

    changes to read method to support a list of registers or variables

--- device.hpp
+++ device.hpp
@@ -1,9 +1,20 @@
 #ifndef device_hpp
 #define device_hpp
 
+template<typename Device>
+struct dread
+{
+  Device * device;
+  dread(Device * device_) : device(device_) {}
+  void operator()()
+  
+}
+
 template<typename REGISTER_LIST, typename ACTUAL>
 struct Device : public HoldClass<REGISTER_LIST>::type
 {
+  typedef Device<REGISTER_LIST, ACTUAL> ThisType;
+
   template<typename REGISTER>
   REGISTER & reg()
   {
@@ -31,6 +42,15 @@ struct Device : public HoldClass<REGISTER_LIST>::type
   template<typename X>
   void read()
   {
+    typedef boost::mpl::as_sequence<X>::type VariableList;
+    typedef boost::mpl::fold_backward<VariableList, boost::mpl::list<>, 
+      boost::mpl::apply<boost::mpl::_2, boost::mpl::_1> >
+    >::type RegisterList;
+    
+    typedef unsorted_unique<RegisterList>::type y;
+    
+    boost::mpl::for_each<y, boost::mpl::identity<boost::mpl::_1>(dread<ThisType>(&device));
+    
     X::read(actual());  
   }
   
